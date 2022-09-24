bbe45293a8837a1f3f1a020efcf53dd20073b252
Семен Шишкин <shishkinsn.20@edu.ystu.ru>
2022-09-24 18:35:50 +0300

    хз

--- CesarCipher/Forms/FreqAnalysis.cs
+++ CesarCipher/Forms/FreqAnalysis.cs
@@ -1,18 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
+﻿using CesarCipher.Classes;
 using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
 
 namespace CesarCipher.Forms
 {
     public partial class FreqAnalysis : Form
     {
         private char[] freqList = "оеаинтсрвлкмдпуяыьгзбчйхжшюцщэфъё".ToCharArray();
+        private char[] alphabet = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя".ToCharArray();
+        private char[] freqMsg;
+        private int _shift;
+        private Dictionary<char, int> freqMsgList = new Dictionary<char, int>();
+        
         public FreqAnalysis()
         {
             InitializeComponent();
@@ -22,25 +20,60 @@ namespace CesarCipher.Forms
         {
             if (string.IsNullOrWhiteSpace(tb_msg.Text)) return;
             tb_result.Text = "";
-
+            FreqInMsg();
+            FreqAnalisys();
+            
         }
 
         private void FreqInMsg()
         {
-            for(int i = 0; i < freqList.Length; i++)
+            var res = tb_msg.Text.ToLower().GroupBy(x => x).ToDictionary(x => x.Key, x => x.Count());
+            foreach (var item in res.OrderByDescending(x => x.Value))
             {
-                int letter = (char)freqList[i];
-                int tmp = 0;
-                int count1 = 0;
-                int count2 = 0;
-                for (int j = 0; j < freqList.Length; j++)
-                {
-                    if (letter == freqList[j]) count1++; count2 = count1;
-                }
-                //zakonchil zdes
+                freqMsgList.Add(item.Key, item.Value);
             }
+
+            string str = "";
+            foreach (var k in freqMsgList.Keys)
+            {
+                str += char.ConvertFromUtf32((int)k);
+            }
+            freqMsg = str.ToCharArray();
         }
 
+        private void FreqAnalisys()
+        {
+            int msgLetter = -1;
+            int ordLetter = -1;
+            int j = 0;
+            for(int i = 0; i < freqMsg.Length; i++)
+            {
+                
+                //if (msgLetter >= 0 && ordLetter >= 0) break;
+                for (; j < freqList.Length; j++)
+                {
+                    if (msgLetter >= 0 && ordLetter >= 0) {
+                        msgLetter = -1;
+                        ordLetter = -1;
+                        break;
+                    } 
+                    for (int k = 0; k< alphabet.Length; k++)
+                    {
+                        if (freqMsg[i] == alphabet[k]) msgLetter = k; //буква у залетает в переменную
+                        else if (freqList[j] == alphabet[k]) ordLetter = k; //буква о залетает в переменную
+                        if (msgLetter >= 0 && ordLetter >= 0) 
+                        {
+                            _shift = Math.Abs(ordLetter - msgLetter) % 32;
+                            tb_result.Text += $"Сдвиг={_shift}, расшифровка: " + Encrypter.OnDecrypt(tb_msg.Text, _shift) + ";" + Environment.NewLine;
+                            break;
+                        } 
+                    }
+                }
+                
+            }
+
+            
+        }
         #region Сохранение и загрузка из файла
         private void OnOpenFile(object sender, EventArgs e)
         {
